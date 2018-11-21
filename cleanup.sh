5a7959f5d81acfb3f6f029b5ac208cf28b38224a
Kenta Togashi <4276550+kentatogashi@users.noreply.github.com>
2018-11-21 22:41:57 +0900

    fix typo

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,3 @@
-ps auxwww | grep -E '(^Ccker-proxy|containerd-shim)' | awk '{print $2}' | xargs sudo kill -9
+#!/bin/sh
+ps auxwww | grep -E '(^docker-proxy|containerd-shim)' | awk '{print $2}' | xargs sudo kill -9
 sudo docker rm -f $(docker ps -qa)
